658e93d7d6b835f5a875b5eb89c936754720bac2
Tetiana Kubyshkina <tanyasha_@ukr.net>
2026-04-20 13:52:36 +0300

    upload not found page

---
+++ app/profile/[id]/page.tsx
@@ -0,0 +1,9 @@
+import { notFound } from "next/navigation";
+
+export default async function ProfilePage({ params }: { params: { id: string } }) {
+    const user = await getUser(params.id);
+    if (!user) {
+        notFound();
+    }
+    return <div>{user.name}</div>
+}
